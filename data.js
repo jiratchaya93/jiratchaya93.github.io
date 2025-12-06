a0cf55161001e38f713709a7b5137daa37600875
jiratchaya93 <jiratchaya1601@gmail.com>
2025-12-06 14:14:00 +0700

    update

---
+++ data.js
@@ -0,0 +1,7 @@
+export const Infomation = {
+    Name: "Jiratchaya",
+    Suname: "Makul",
+    Age: "16 years old",
+    Contact: "gxm2.jkm_",
+    
+}
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello")
+import {Infomation} from "./data.js"
+
+const container = document.getElementById("container")
+console.log(container)
+
+animes.forEach(anime => {
+    let anime_div = document.createElement("div")
+    anime_div.setAttribute("class","anime_div")
+    console.log(anime)
+
+    // episode amount
+    let episode_amount = document.createElement("div")
+    episode_amount.setAttribute("class","episode_amount")
+    episode_amount.innerHTML = `${anime.episode_amount}ตอน`
+    anime_div.appendChild(episode_amount)
+
+
+    //img
+    let img_anime = document.createElement("img")
+    img_anime.setAttribute("src",anime.anime_url)
+    anime_div.appendChild(img_anime)
