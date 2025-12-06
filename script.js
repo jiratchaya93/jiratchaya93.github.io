9a2486e20d14c0c4c70226a2f3188fa57218c4d0
jiratchaya93 <jiratchaya1601@gmail.com>
2025-12-06 15:47:49 +0700

    update

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-import {Infomation} from "./data.js"
+import {Infomations} from "./data.js"
 
 const container = document.getElementById("container")
 console.log(container)
 
-animes.forEach(anime => {
-    let anime_div = document.createElement("div")
-    anime_div.setAttribute("class","anime_div")
-    console.log(anime)
+Infomations.forEach(Infodata => {
+    let Infomation_div = document.createElement("div")
+    Infomation_div.setAttribute("class","Infomation_div")
+    console.log(Infomation_div)
 
-    // episode amount
-    let episode_amount = document.createElement("div")
-    episode_amount.setAttribute("class","episode_amount")
-    episode_amount.innerHTML = `${anime.episode_amount}ตอน`
-    anime_div.appendChild(episode_amount)
+    //Certificate
 
+    let Certificate = document.createElement("p")
+    Certificate.setAttribute("class","Certificate")
+    Certificate.innerHTML = (Infodata.Certificate)
+    Infomation_div.appendChild(Certificate)
+
+    // Day
+    let Day = document.createElement("p")
+    Day.setAttribute("class","Day")
+    Day.innerHTML = `${Infodata.Day}`
+    
+
+    // Month
+    let Month = document.createElement("p")
+    Month.setAttribute("class","Month")
+    Month.innerHTML = (Infodata.Month)
+    
+
+
+    //Year
+
+    let Year = document.createElement("p")
+    Year.setAttribute("class","Year")
+    Year.innerHTML = (Infodata.Year)
+    
+
+    
+
+    let tub = document.createElement("p")
+    tub.setAttribute("class","tub")
+    tub.innerHTML = `${Infodata.Day} / ${Infodata.Month} / ${Infodata.Year}`
+
+    Infomation_div.appendChild(tub)
 
     //img
-    let img_anime = document.createElement("img")
-    img_anime.setAttribute("src",anime.anime_url)
-    anime_div.appendChild(img_anime)
+    let img_certificate = document.createElement("img")
+    img_certificate.setAttribute("src",Infodata.Img)
+    Infomation_div.appendChild(img_certificate)
+
+    img_certificate.addEventListener("mouseover",() => {
+        img_certificate.style.width='290px'
+        img_certificate.style.transition = "all 0.4s ease";
+    })
+
+    img_certificate.addEventListener("mouseout",() => {
+        img_certificate.style.width='260px'
+        img_certificate.style.transition = "all 0.4s ease";
+    })
+
+
+    
+    container.appendChild(Infomation_div)
+})
+
+
+
+
